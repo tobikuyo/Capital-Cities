eff051ab09c24d70c245a6580bb0b077f5f8f779
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-09 19:28:19 +0100

    First Commit

---
+++ CapitalCities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CapitalCities
+//
+//  Created by Tobi Kuyoro on 09/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
