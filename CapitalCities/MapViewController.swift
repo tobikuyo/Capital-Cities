3134a0982cb7159c739621ec3db649700bd9306b
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-09 20:24:03 +0100

    Created MapViewController and Connected Outlet

---
+++ CapitalCities/MapViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MapViewController.swift
+//  CapitalCities
+//
+//  Created by Tobi Kuyoro on 09/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet weak var mapView: MKMapView!
+
+    // MARK: - Properties
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Methods
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
